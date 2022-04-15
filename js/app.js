83bfed72ab90e12c9a3df0a12dd55a0090f8bdbc
Chinganshih <76547828+Chinganshih@users.noreply.github.com>
2022-04-15 11:23:16 -0400

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,201 @@
+/**
+ * WEB222 – Final Assessment
+ *
+ * I declare that this assignment is my own work in accordance with
+ * Seneca Academic Policy. No part of this assignment has been
+ * copied manually or electronically from any other source
+ * (including web sites) or distributed to other students.
+ *
+ * Please update the following with your information:
+ *
+ *      Name:       Ching-An, Shih
+ *      Student ID: 148221195
+ *      Date:       2022-04-15
+ */
+
+function validateInput(e) {
+    let isInvalid = false;
+    const value = e.target.value.trim();
+    switch (e.target.name) {
+        case "fname":
+            isInvalid = value === "";
+            break;
+
+        case "lname":
+            isInvalid = value === "";
+            break;
+
+        case "phone":
+            isInvalid = value === "";
+            break;
+        case "email":
+            var regex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+            var pr = regex.test(e.target.value);
+            isInvalid = value === "" || !pr;
+            break;
+
+        case "address":
+            isInvalid = value === "";
+            break;
+
+        case "city":
+            isInvalid = value === "";
+            break;
+
+        case "postalcode":
+            var regex = /^[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d$/;
+            var pr = regex.test(e.target.value);
+            isInvalid = value === "" || !pr;
+            break;
+
+        case "question":
+            isInvalid = value === "";
+            break;
+
+        case "question":
+            isInvalid = value === "";
+            break;
+
+        case "other":
+            isInvalid = value === "";
+            break;
+    }
+    e.target.classList.toggle("invalid", isInvalid);
+    return isInvalid;
+}
+
+// 1.	Create an event handler to run when the page is loaded.
+window.onload = function() {
+    var formData = {};
+    var contact = document.querySelector(".contact");
+    var fname = document.querySelector("#fname");
+    var lname = document.querySelector("#lname");
+    var phone = document.querySelector("#phone");
+    var email = document.querySelector("#email");
+    var address = document.querySelector("#address");
+    var email = document.querySelector("#email");
+    var postalcode = document.querySelector("#postalcode");
+    var hiring = document.querySelector('#hiring');
+    var hourlyrate = document.querySelector('#hourlyrate');
+    var question = document.querySelector("#question");
+    var other = document.querySelector("#other");
+    var comment = document.querySelector("#comment");
+    var submit = document.querySelector("#submit");
+
+    contact.addEventListener("click", function() {
+        document.querySelector(".content").style.visibility = "visible";
+        document.querySelector(".content").removeAttribute("hidden");
+    })
+
+    question.addEventListener("click", function(e) {
+        hiring.checked = false;
+        comment.checked = false;
+
+    })
+
+    comment.addEventListener("click", function(e) {
+        question.checked = false;
+        hiring.checked = false;
+
+    })
+
+    hiring.addEventListener("click", function(e) {
+        var hourlylabel = document.createElement("label");
+        var hourlyinput = document.createElement("input");
+
+        hourlyrate.appendChild(hourlylabel);
+        hourlyrate.appendChild(hourlyinput);
+        hourlylabel.innerText = "Hourly Rate (Ex. 14.5)";
+        hourlyinput.id = "hourlyinput";
+        hourlyinput.type = "text";
+        hourlyinput.setAttribute("required", "");
+        hourlyinput.required = true;
+
+
+        question.checked = false;
+        comment.checked = false;
+
+        question.addEventListener("click", function(e) {
+            hourlyrate.removeChild(hourlylabel);
+            hourlyrate.removeChild(hourlyinput);
+
+        })
+
+        comment.addEventListener("click", function(e) {
+            hourlyrate.removeChild(hourlylabel);
+            hourlyrate.removeChild(hourlyinput);
+
+        })
+    })
+
+    submit.addEventListener("click", function(e) {
+        var hourrateEpy = 0;
+        var hourlyinput = document.querySelector('#hourlyinput');
+        var url = "https://httpbin.org/post?key=formdata";
+        var xhr = new XMLHttpRequest();
+        e.preventDefault();
+
+        // check validation
+        if (hourlyinput) {
+            const isInvalid = hourlyinput.value === "" || !isFinite(hourlyinput.value);
+            hourlyinput.classList.toggle("invalid", isInvalid);
+            (isInvalid) ? hourrateEpy = 1: 0;
+        }
+        const isInvalid = validateInput({ target: fname }) | validateInput({ target: lname }) | validateInput({ target: phone }) | validateInput({ target: email }) | validateInput({ target: address }) | validateInput({ target: city }) |
+            validateInput({ target: postalcode }) | validateInput({ target: question }) | validateInput({ target: other });
+
+        if (!isInvalid && !hourrateEpy) {
+            formData.fname = fname.value;
+            formData.lname = lname.value;
+            formData.phone = phone.value;
+            formData.email = email.value;
+            formData.address = address.value;
+            formData.city = city.value;
+            formData.postalcode = postalcode.value;
+            formData.other = other.value;
+
+            if (question.checked) {
+                formData.question_about = question.value;
+            } else if (comment.checked) {
+                formData.question_about = comment.value;
+            } else {
+                formData.question_about = hiring.value;
+                formData.hourRate = hourlyinput.value;
+            }
+
+            // display information via window.confirm
+            if (window.confirm("Please check your information below.\n" +
+                    "First Name: " + fname.value + "\n" +
+                    "Last Name: " + lname.value + "\n" +
+                    "Phone: " + phone.value + "\n" +
+                    "Email: " + email.value + "\n" +
+                    "Address: " + address.value + "\n" +
+                    "City: " + city.value + "\n" +
+                    "Postal Code: " + postalcode.value + "\n" +
+                    "Question About: " + formData.question_about + "\n" +
+                    "Other: " + other.value + "\n")) window.confirm("Submit successfully! Thank you for your feedback!");
+
+            // clear input data
+            fname.value = "";
+            lname.value = "";
+            phone.value = "";
+            email.value = "";
+            address.value = "";
+            city.value = "";
+            postalcode.value = "";
+            question.value = "";
+            other.value = "";
+            if (hourlyinput) hourlyinput.value = "";
+        } else alert("Invalid information, Please check again before you submit!");
+
+        // POST TO Server
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState == 4 && xhr.status == 200) {
+                console.log(xhr.responseText);
+            }
+        }
+        xhr.open("POST", url);
+        xhr.send(JSON.stringify(formData));
+
+    })
+};
